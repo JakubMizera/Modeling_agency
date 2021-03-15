b3efee508f780966b71db3ac8374c39089dd7d0c
JakubMizera <kubamiz53@gmail.com>
2021-03-15 13:41:09 +0100

    main content v1.0

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "../scss/main.scss";
 
 import {Header} from "./main_page/header"
+import {Content} from "./main_page/content"
 
 
 const App = () => {
     return (
         <div className="pageContainer">
             <Header/>
+            <Content/>
         </div>
     )
 }
--- app/js/main_page/content.js
+++ app/js/main_page/content.js
@@ -3,7 +3,30 @@ import React from "react";
 const Content = () => {
     return (
         <main>
-            
+            <div className='photobox'>
+                <a href='#'>
+                    <div className='phototile'>
+                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
+                    </div>
+                </a>
+                <a href='#'>
+                    <div className='phototile'>
+                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
+                    </div>
+                </a>
+                <a href='#'>
+                    <div className='phototile'>
+                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
+                    </div>
+                </a>
+                <a href='#'>
+                    <div className='phototile'>
+                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
+                    </div>
+                </a>
+            </div>
         </main>
     )
 };
+
+export {Content};
