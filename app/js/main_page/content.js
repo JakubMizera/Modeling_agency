431d36cc65cbc83ccd1660b4336ecda45c0f0e4e
JakubMizera <kubamiz53@gmail.com>
2021-03-16 10:55:56 +0100

    main v1.1

--- app/js/main_page/content.js
+++ app/js/main_page/content.js
@@ -3,27 +3,29 @@ import React from "react";
 const Content = () => {
     return (
         <main>
+            <div className='main_info'>
+                <h1>Modelki</h1>
+            </div>
             <div className='photobox'>
-                <a href='#'>
-                    <div className='phototile'>
-                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
-                    </div>
-                </a>
-                <a href='#'>
-                    <div className='phototile'>
-                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
-                    </div>
-                </a>
-                <a href='#'>
-                    <div className='phototile'>
-                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
-                    </div>
-                </a>
-                <a href='#'>
-                    <div className='phototile'>
-                        <img src='../images/Photomodel3.jpg' alt='Kasia'/>
-                    </div>
-                </a>
+                <div className='phototile'>
+                    <span className='name'>Kasia</span>
+                    <a href='#'>
+                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
+                    </a>
+                </div>
+                <div className='phototile'>
+                    <a href='#'>
+                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
+                    </a>
+                </div>
+                <div className='phototile'>
+                    <a href='#'>
+                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
+                    </a>
+                </div>
+                <div className='phototile'>
+                    
+                </div>
             </div>
         </main>
     )
