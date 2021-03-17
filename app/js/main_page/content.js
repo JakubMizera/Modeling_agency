4dc466aad92584eee4bfbc8a520d6d814804cd44
JakubMizera <kubamiz53@gmail.com>
2021-03-17 12:52:24 +0100

    main page photos transform

--- app/js/main_page/content.js
+++ app/js/main_page/content.js
@@ -1,34 +1,67 @@
 import React from "react";
 
 const Content = () => {
-    return (
-        <main>
-            <div className='main_info'>
-                <h1>Modelki</h1>
-            </div>
-            <div className='photobox'>
-                <div className='phototile'>
-                    <span className='name'>Kasia</span>
-                    <a href='#'>
-                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
-                    </a>
-                </div>
-                <div className='phototile'>
-                    <a href='#'>
-                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
-                    </a>
-                </div>
-                <div className='phototile'>
-                    <a href='#'>
-                        <img src='../images/Photomodel2.jpg' alt='Kasia'/>
-                    </a>
-                </div>
-                <div className='phototile'>
-                    
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="container">
+        <div className="main_info">
+          <h1>Modelki</h1>
+        </div>
+        <div className="photobox">
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel3.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Kasia</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Zosia</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Tatiana</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Małgorzata</span>
+          </div>
+        </div>
+        <div className="photobox">
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Kasia</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Zosia</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Tatiana</span>
+          </div>
+          <div className="phototile">
+            <a href="#">
+              <img src="../images/Photomodel2.jpg" alt="Kasia" />
+            </a>
+            <span className="name">Małgorzata</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
-export {Content};
+export { Content };
--- app/js/main_page/header.js
+++ app/js/main_page/header.js
@@ -1,38 +1,40 @@
 import React from "react";
 
 const Header = () => {
-    return (
-        <header>
-            <div className='header_container'>
-                <div className='logo'>
-                    <a href='#'>
-                        <img src='../images/logo.png' alt='pagelogo'/>
-                    </a>
-                </div>
-                <nav>
-                    <div className='main_menu'>
-                        <ul>
-                            <li>
-                                <a href='#'>
-                                    <span className='menuBTN'>MODELKI</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <span className='menuBTN'>CASTING</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <span className='menuBTN'>KONTAKT</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+  return (
+    <header>
+      <div className="container">
+        <div className="header_container">
+          <div className="logo">
+            <a href="#">
+              <img src="../images/logo.png" alt="pagelogo" />
+            </a>
+          </div>
+          <nav>
+            <div className="main_menu">
+              <ul>
+                <li>
+                  <a href="#">
+                    <span className="menuBTN">MODELKI</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span className="menuBTN">CASTING</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span className="menuBTN">KONTAKT</span>
+                  </a>
+                </li>
+              </ul>
             </div>
-        </header>
-    )
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
-export {Header};
+export { Header };
