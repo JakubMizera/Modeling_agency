0d0b62c2875585c96fb46261ffe394ce18e3cb8a
JakubMizera <kubamiz53@gmail.com>
2021-04-07 12:42:20 +0200

    forms labels

--- app/js/elements/Form.js
+++ app/js/elements/Form.js
@@ -1,30 +1,82 @@
 import React, { useState } from "react";
 
 const Form = () => {
-    const [form, setForm] = useState(
-        {name:"", 
-        surname:"", 
-        email: "",
-        phoneNumber: ""}
-        );
+  const [form, setForm] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    phoneNumber: "",
+    city: "",
+    birthDate: "",
+  });
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setForm(prevState => {
-            return {
-                ... prevState,
-                [name]: value
-            }
-        });
-    };
-    return (
-        <form>
-            <input type='text' name='name' value={form.name} onChange={handleChange}/>
-            <input type='text' name='surname' value={form.surname} onChange={handleChange}/>
-            <input type='email' name='email' value={form.email} onChange={handleChange}/>
-            <input type='number' name='phoneNumber' value={form.phoneNumber} onChange={handleChange}/>
-        </form>
-    )
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+  return (
+    <form>
+      <label>
+        Imię
+        <input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Nazwisko
+        <input
+          type="text"
+          name="surname"
+          value={form.surname}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        E-mail
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Telefon
+        <input
+          type="text"
+          name="phoneNumber"
+          value={form.phoneNumber}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Miasto
+        <input
+          type="text"
+          name="city"
+          value={form.city}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Data urodzenia
+        <input
+          type="date"
+          name="birthdate"
+          value={form.birthDate}
+          onChange={handleChange}
+        />
+      </label>
+    </form>
+  );
+};
 
-export {Form};
+export { Form };
