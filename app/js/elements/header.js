ec5f9755d1ed33ac9600d7258b5d93505f4907a3
JakubMizera <kubamiz53@gmail.com>
2021-04-08 13:05:50 +0200

    media queries header

--- app/js/elements/header.js
+++ app/js/elements/header.js
@@ -5,48 +5,46 @@ import { FaFacebookSquare } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
-      <div className="container">
-        <div className="header_container">
-          <div className="logo">
-            <a href="#">
-              <img src="../images/logo.png" alt="pagelogo" />
-            </a>
-          </div>
-          <nav>
-            <div className='hamburger'>
-              <div className='line'></div>
-              <div className='line'></div>
-              <div className='line'></div>
-            </div>
-            <ul>
-              <a href="#">
-                <li>
-                  <span className="menuBTN">MODELKI</span>
-                </li>
-              </a>
-              <a href="#">
-                <li>
-                  <span className="menuBTN">CASTING</span>
-                </li>
-              </a>
-              <a href="#">
-                <li>
-                  <span className="menuBTN">KONTAKT</span>
-                </li>
-              </a>
-              <a href="#">
-                <li>
-                  <FaInstagram/>
-                </li>
-              </a>
-              <a href="#">
-                <li>
-                  <FaFacebookSquare/>
-                </li>
-              </a>
-            </ul>
-          </nav>
+      <div className="header_container">
+        <div className="logo">
+          <a href="#">
+            <img src="../images/logo.png" alt="pagelogo" />
+          </a>
         </div>
+        <nav>
+          <div className="hamburger">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <ul>
+            <a href="#">
+              <li>
+                <span className="menuBTN">MODELKI</span>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <span className="menuBTN">CASTING</span>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <span className="menuBTN">KONTAKT</span>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <FaInstagram />
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <FaFacebookSquare />
+              </li>
+            </a>
+          </ul>
+        </nav>
       </div>
     </header>
   );
