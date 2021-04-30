75c2857196f8e1c3989d3256d4ecf6bd94fdae81
JakubMizera <kubamiz53@gmail.com>
2021-04-30 15:16:21 +0200

    footer name change

--- app/js/elements/footer.js
+++ app/js/elements/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer>
-            <p><a href='#'>Polityka Prywatności</a> / Copyright © 2021 Wojtek Models. All Rights Reserved.</p>
+            <p><a href='#'>Polityka Prywatności</a> / Copyright © 2021 Divine Shapes. All Rights Reserved.</p>
         </footer>
     );
 }
